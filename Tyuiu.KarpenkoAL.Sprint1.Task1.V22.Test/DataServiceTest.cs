fdb6c5eae85013df570d2a5801897bade6f9b873
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-13 22:23:51 +0500

    Task1

---
+++ Tyuiu.KarpenkoAL.Sprint1.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KarpenkoAL.Sprint1.Task1.V22.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint1.Task1.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
