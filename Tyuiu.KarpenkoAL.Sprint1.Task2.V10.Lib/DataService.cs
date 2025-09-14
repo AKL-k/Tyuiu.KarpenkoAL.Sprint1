ba8fd6fc9d312dd3a82ef0c2a16afccc9d515c5c
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-14 20:44:47 +0500

    Task2

---
+++ Tyuiu.KarpenkoAL.Sprint1.Task2.V10.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KarpenkoAL.Sprint1.Task2.V10.Lib
+{
+    public class DataService : ISprint1Task2V10
+    {
+        public double ConvertMetreToInchs(int value)
+        {
+            return Math.Round(value * 39.3700787402, 3); 
+        }
+    }
+}
+
