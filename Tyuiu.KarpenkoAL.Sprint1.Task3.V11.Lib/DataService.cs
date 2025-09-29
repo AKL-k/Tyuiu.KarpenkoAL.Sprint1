7ad0af79695fa2b5f969a6ca34111266334b061d
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-29 08:04:33 +0500

    Task3

--- Tyuiu.KarpenkoAL.Sprint1.Task3.V11.Lib/DataService.cs
+++ Tyuiu.KarpenkoAL.Sprint1.Task3.V11.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KarpenkoAL.Sprint1.Task3.V11.Lib
     {
         public double TriangleArea(double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            return Math.Round(((((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2))) / 2) * -1), 3);
+            return Math.Round(Math.Abs(((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2))) / 2), 3);
         }
     }
 }
